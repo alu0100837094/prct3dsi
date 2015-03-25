a3f9c3626fefca258cf51333e7be62e8a26babf4
J.Joaquin <quinoescobar@gmail.com>
2015-03-25 10:52:14 +0000

    Cositas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+   #retorna el titulo completo on a pear-page basis
+	def full_title(page_title = '')
+		base_title="Tutorial Ruby on Rails"
+		if page_title.empty?
+			base_title
+		else
+			"#{page_title} | #{base_title}"
+		end
+	end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -60,6 +60,7 @@ Rails.application.routes.draw do
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
 end
 
 end
